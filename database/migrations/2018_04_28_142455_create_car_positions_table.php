cad585ac8c2dfc5b114fa7786deb8d3513e01e9b
Phelipe <phelipeevangelistasimimdiniz@gmail.com>
2018-05-02 19:58:53 -0300

    adicionando migrates para salvar o token

---
+++ database/migrations/2018_04_28_142455_create_car_positions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarPositionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('car_positions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('conectado');
+            $table->integer('deconectado');
+            $table->integer('acionamentos');
+            $table->float('distancia_percorrida', 8, 2);
+            $table->float('vel_max', 8, 2);
+            $table->float('vel_med', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('car_positions');
+    }
+}
